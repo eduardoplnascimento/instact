af52483ce210c3fef7addd9d4afe66d3a5651d42
Eduardo Nascimento <eduardoplnascimento@gmail.com>
2023-01-16 21:57:15 -0300

    Adicionada funcionalidade de criacao

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function dashboard()
     {
-        return view('dashboard');
+        $posts = Post::all();
+
+        return view('dashboard', compact('posts'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -18,3 +19,8 @@ use App\Http\Controllers\UserController;
 Route::get('/', [HomeController::class, 'dashboard']);
 Route::get('/login', [UserController::class, 'login'])->name('login');
 Route::get('/register', [UserController::class, 'register']);
+Route::post('/signin', [UserController::class, 'signin']);
+Route::post('/signup', [UserController::class, 'signup']);
+
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts/store', [PostController::class, 'store']);
